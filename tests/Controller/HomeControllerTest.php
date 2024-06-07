d19b0cca835638ceea4ca43bdfdbc65f4fa699a1
Abdouni Abdelkarim <abdounikarim@gmail.com>
2024-06-07 05:28:21 +0200

    Test home

---
+++ tests/Controller/HomeControllerTest.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+final class HomeControllerTest extends WebTestCase
+{
+    public function testHomepage(): void
+    {
+        $client = static::createClient();
+        $client->request('GET', '/');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h1', 'Hello from Symfony!👋');
+    }
+}
